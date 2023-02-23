ef3a93dc9bc8deb34efebbc278a0ab740763733a
Rahul L <98452784+Rahul-0018@users.noreply.github.com>
2023-02-23 15:57:40 +0530

    Delete Day-3/trail2 directory

--- Day-3/trail2/src/main/java/com/example/demo/Repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
-}
